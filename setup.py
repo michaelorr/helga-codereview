4f8d116c54cc24886a60cf45ae3f0ae0aefb2c09
Michael Orr <michael@orr.co>
2015-10-31 10:53:09 -0400

    Support pip versions

--- setup.py
+++ setup.py
@@ -1,30 +1,57 @@
+import functools
+import pkg_resources
+
 from setuptools import setup, find_packages
+
+from pip.req import parse_requirements as parse_reqs
+
 
 version = '0.1'
 
+
+# Compatibility with older versions of pip
+pip_dist = pkg_resources.get_distribution('pip')
+pip_version = tuple(map(int, pip_dist.version.split('.')))
+
+# Use a base partial that will be updated depending on the version of pip
+parse_requirements = functools.partial(parse_reqs, options=None)
+
+if pip_version < (1, 2):
+    # pip versions before 1.2 require an options keyword for using it outside
+    # of invoking a pip shell command
+    from pip.baseparser import parser
+    parse_requirements.keywords['options'] = parser.parse_args()[0]
+
+if pip_version >= (1, 5):
+    # pip 1.5 introduced a session kwarg that is required in later versions
+    from pip.download import PipSession
+    parse_requirements.keywords['session'] = PipSession()
+
+
 setup(name="helga-codereview",
-      version=version,
-      description=('Reviewboard code reviews'),
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: IRC',
-                   'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Software Development :: Libraries :: Python Modules',
-                   'Topic :: IRC Bots'],
-      keywords='irc helga reviewboard',
-      author='Shaun Duncan',
-      author_email='shaun.duncan@gmail.com',
-      url='https://github.com/shaunduncan/helga-codereview',
-      license='MIT',
-      install_requires=[
-          'RBTools==0.5.7',
-          'flake8==2.1.0',
-      ],
-      packages=find_packages(),
-      entry_points = dict(
-          helga_plugins=[
-              'codereview= helga_codereview:codereview',
-          ],
-      ),
+    version=version,
+    description=('Reviewboard code reviews'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: IRC',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: IRC Bots'
+    ],
+    keywords='irc helga reviewboard',
+    author='Shaun Duncan',
+    author_email='shaun.duncan@gmail.com',
+    url='https://github.com/shaunduncan/helga-codereview',
+    license='MIT',
+    install_requires=[
+        str(req.req) for req in parse_requirements('requirements.txt')
+    ],
+    packages=find_packages(),
+    entry_points = dict(
+        helga_plugins=[
+            'codereview= helga_codereview:codereview',
+        ],
+    ),
 )
